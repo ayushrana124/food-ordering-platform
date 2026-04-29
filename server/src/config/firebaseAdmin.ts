22aa4009d1d96569e3cb890f92668638f1c884bf
ayushrana124 <ayushrana73107@gmail.com>
2026-04-29 09:05:01 +0530

    Fix Build errors

--- server/src/config/firebaseAdmin.ts
+++ server/src/config/firebaseAdmin.ts
@@ -1,13 +1,10 @@
 import admin from 'firebase-admin';
-import path from 'path';
-
-// Initialize Firebase Admin SDK with service account key
-const serviceAccountPath = path.resolve(__dirname, '../../', 'buntypizza-442b0-firebase-adminsdk-fbsvc-1bb32e1b69.json');
-
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const serviceAccount = require(serviceAccountPath);
 
 if (!admin.apps.length) {
+    const raw = process.env.FIREBASE_SERVICE_ACCOUNT!;
+
+    const serviceAccount = JSON.parse(raw);
+
     admin.initializeApp({
         credential: admin.credential.cert(serviceAccount),
     });
